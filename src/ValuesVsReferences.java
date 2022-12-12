f43e9f876c8edb1b7dbc7922be72ff218559f27d
bartwietecki <bartwietecki@gmail.com>
2022-12-12 20:18:53 +0100

    Dodano przykłady obiekt vs wartość

---
+++ src/ValuesVsReferences.java
@@ -0,0 +1,14 @@
+public class ValuesVsReferences {
+    public static void main(String[] args) {
+        // wartości
+        int a = 10;
+        boolean b = true;
+
+
+        // obiekty
+        String powitanie = "Hejo";
+
+        //tablica sama w sobie jest obiektem ogólnie
+        double[] rzeczywiste = new double[5];
+    }
+}
