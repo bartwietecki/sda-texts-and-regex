4786b24a1a307c6b3479c885d1a09f100865f8b5
bartwietecki <bartwietecki@gmail.com>
2022-12-12 20:44:52 +0100

    Dodano rozwiązanie dla zadania z falą

---
+++ src/Problem019.java
@@ -0,0 +1,73 @@
+/*
+ZADANIE 19.
+Napisz program, który pobierze od użytkownika liczbę dodatnią (typu int) i narysuje falę
+o zadanej długości i wysokości 4 linijek, zgodnie z poniższym schematem (puste pola uzupełniaj spacjami):
+np dla długości dwa to:
+*      **      *
+ *    *  *    *
+  *  *    *  *
+   **      **
+a dla długości trzy to:
+*      **      **      *
+ *    *  *    *  *    *
+  *  *    *  *    *  *
+   **      **      **
+
+ */
+
+import java.util.Scanner;
+
+public class Problem019 {
+
+    private static final String FIRST_ROW_ELEMENT = "*      *";
+    private static final String SECOND_ROW_ELEMENT = " *    * ";
+    private static final String THIRD_ROW_ELEMENT = "  *  *  ";
+
+    private static final String FOURTH_ROW_ELEMENT = "   **   ";
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj liczbę dodatnią.");
+        int waveLength = scan.nextInt();
+        printRow(waveLength, FIRST_ROW_ELEMENT);
+        printRow(waveLength, SECOND_ROW_ELEMENT);
+        printRow(waveLength, THIRD_ROW_ELEMENT);
+        printRow(waveLength, FOURTH_ROW_ELEMENT);
+    }
+
+    private static void printRow(int waveLength, String rowElement){
+        for(int i=0; i<waveLength;i++){
+            System.out.println(rowElement);
+        }
+        System.out.println();
+    }
+}
+
+
+
+/*
+public class Problem019 {
+
+    private static final String FIRST_ROW_ELEMENT = "*      *";
+    private static final String SECOND_ROW_ELEMENT = " *    * ";
+    private static final String THIRD_ROW_ELEMENT = "  *  *  ";
+    private static final String FOURTH_ROW_ELEMENT = "   **   ";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int waveLength = scanner.nextInt();
+        printRow(waveLength, FIRST_ROW_ELEMENT);
+        printRow(waveLength, SECOND_ROW_ELEMENT);
+        printRow(waveLength, THIRD_ROW_ELEMENT);
+        printRow(waveLength, FOURTH_ROW_ELEMENT);
+    }
+
+    private static void printRow(int waveLength, String rowElement) {
+        for(int i=0; i< waveLength;i++){
+            System.out.print(rowElement);
+        }
+        System.out.println();
+    }
+
+}
+ */
