a5bf160845bc905c88246b375a709aee7297458d
bartwietecki <bartwietecki@gmail.com>
2022-12-13 18:25:56 +0100

    Dodano rozwiązanie dla Regex001

---
+++ src/regex/Regex001.java
@@ -0,0 +1,30 @@
+package regex;
+
+/*
+Napisz Regex,
+który dopasowuje ciąg znaków, w który zaczyna się od 'p',
+ po którym następuje cokolwiek (dowolny znak, dowolną liczbę razy), kończąc na 'q'.
+ */
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex001 {
+            private static final Pattern pattern = Pattern.compile("p.*q");
+
+            public static void main(String[] args) {
+                validate("pdfdfgdfsg");
+                validate("pq");
+                validate("p5235325236q");
+                validate("pqqqqq");
+            }
+
+            private static void validate(String text) {
+                Matcher matcher = pattern.matcher(text);
+                if (matcher.matches()) {
+                    System.out.println("Prawda dla tekstu: " + text);
+                } else {
+                    System.out.println("Fałsz dla tekstu: " + text);
+                }
+            }
+        }
